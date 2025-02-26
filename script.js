ffd0c92658403c737f436ef028be50d56dcbc715
Onjospiat <guilherme.chini.gani@gmail.com>
2025-02-26 09:36:24 -0300

    Adicionadas as imagens

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+var language = "pt-BR";
+
 function changeText(){
-    var x = document.getElementsByTagName("h1");
-    x[0].innerHTML = "Hello!";
+    let x = document.getElementsByClassName("pt-BR");
+    let y = document.getElementsByClassName("en");
+    console.log(x);
+    console.log(y);
+    let i=0;
+    if(language == "pt-BR"){
+        language = "en";
+        for(i=0;i<x.length;i++){
+            x[i].style.display = "block";
+        }
+        for(i=0;i<y.length;i++){
+            y[i].style.display = "none";
+        }
+    }
+    else if(language == "en"){
+        language = "pt-BR";
+        for(i=0;i<x.length;i++){
+            x[i].style.display = "none";
+        }
+        for(i=0;i<y.length;i++){
+            y[i].style.display = "block";
+        }
+    }
 }
 
 window.changeText = changeText;
+
+
+
+
+
+for(init;condition;update){}
